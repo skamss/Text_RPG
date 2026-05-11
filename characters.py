f844e6969677fcbeb4c98fda8aa2ce6a613620ef
skamss <stas1997.stalker@gmail.com>
2026-05-11 21:22:14 +0300

    Creating class Character

---
+++ characters.py
@@ -0,0 +1,19 @@
+class Character:
+    def __init__(self, name, hp, max_hp, attack, defense):
+        self.name = name
+        self.hp = hp
+        self.max_hp = max_hp
+        self.attack = attack
+        self.defense = defense
+
+    @property
+    def is_alive(self):
+        return self.hp > 0
+
+    def take_damage(self, amount):
+        clear_damage = max(1, amount - self.defense)
+        self.hp = max(0, self.hp - clear_damage)
+        print(f'{self.name} takes {clear_damage} damage! [HP: {self.hp}/{self.max_hp}]')
+
+    def __str__(self):
+        return f'{self.name} [HP: {self.hp}/{self.max_hp}]'
